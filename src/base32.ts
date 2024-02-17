2c9a35cf47cd37d8ac27282caa3d4729f63b3241
esthedebeste <57283066+esthedebeste@users.noreply.github.com>
2024-02-17 17:28:32 +0100

    1.1.0 - Export base32Decode function

--- src/base32.ts
+++ src/base32.ts
@@ -1,6 +1,6 @@
 const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 
-export function base32Encode(data: Uint8Array) {
+export function base32Encode(data: Uint8Array): string {
 	// Adapted from https://github.com/LinusU/base32-encode/blob/d68e91/index.js
 	let bits = 0
 	let value = 0
@@ -17,6 +17,36 @@ export function base32Encode(data: Uint8Array) {
 	}
 
 	if (bits > 0) output += ALPHABET[(value << (5 - bits)) & 31]
+
+	return output
+}
+
+function readChar(char: string) {
+	const index = ALPHABET.indexOf(char)
+	if (index === -1) throw new Error("Invalid character found: " + char)
+	return index
+}
+
+export function base32Decode(input: string): Uint8Array {
+	// Adapted from https://github.com/LinusU/base32-decode/blob/a6601b/index.js
+	input = input.replace(/=+$/, "")
+	const length = input.length
+
+	let bits = 0
+	let value = 0
+
+	let index = 0
+	const output = new Uint8Array(Math.floor((length * 5) / 8))
+
+	for (let index_ = 0; index_ < length; index_++) {
+		value = (value << 5) | readChar(input[index_])
+		bits += 5
+
+		if (bits >= 8) {
+			output[index++] = (value >>> (bits - 8)) & 255
+			bits -= 8
+		}
+	}
 
 	return output
 }
